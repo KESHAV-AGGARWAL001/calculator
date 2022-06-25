042a332234c491dbde787420d07e5937f56de7d6
keshav <aggarwalkeshav001@gmial.com>
2022-06-25 21:51:59 +0530

    making calculator

---
+++ index.js
@@ -0,0 +1,9 @@
+function changetoblue(){
+    document.getElementById("design").style.color = "rgb(153, 240, 230)";
+}
+function changetogreen(){
+    document.getElementById("design").style.color = "rgba(85, 241, 90, 0.806)";
+}
+function changetored(){
+    document.getElementById("design").style.color = "rgba(235, 142, 103, 0.806)";
+}
